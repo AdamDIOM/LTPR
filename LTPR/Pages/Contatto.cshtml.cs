68e647040fe7dbb1ad6278aa8a926cded9f1b3d4
ADAM  DRUMMOND <2126671@chester.ac.uk>
2022-10-26 13:38:07 +0100

    added images to index

---
+++ LTPR/Pages/Contatto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LTPR.Pages
+{
+    public class ContattoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
