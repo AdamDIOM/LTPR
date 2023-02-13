a896cf8c2944c8e7e92334226c7b2ed7a44e3a41
ADAM  DRUMMOND <2126671@chester.ac.uk>
2023-02-13 03:45:45 +0000

    added validation to login/register index

--- LTPR/Pages/Account/Index.cshtml.cs
+++ LTPR/Pages/Account/Index.cshtml.cs
@@ -2,6 +2,7 @@ using LTPR.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography;
 
 namespace LTPR.Pages.Account
@@ -12,6 +13,8 @@ namespace LTPR.Pages.Account
         {
         }
         [BindProperty]
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
 
         private readonly UserManager<ApplicationUser> _userManager;
@@ -23,6 +26,15 @@ namespace LTPR.Pages.Account
 
         public async Task<IActionResult> OnPostAsync()
         {
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (Email == null || Email == "")
+            {
+                return RedirectToPage();
+            }
             // attempts to find user in database, if user exists they are redirected to Login page, otherwise to the Register page. Both pass the entered email address as a parameter to the next page
             var user = await _userManager.FindByNameAsync(Email);
             if(user != null)
