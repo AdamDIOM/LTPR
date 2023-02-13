290e0a9446b76ee3db785aae23f4106dfb1b5228
ADAM  DRUMMOND <2126671@chester.ac.uk>
2023-02-13 04:28:15 +0000

    fixed change user password, added validation

--- LTPR/Pages/Admin/Users/ChangePassword.cshtml.cs
+++ LTPR/Pages/Admin/Users/ChangePassword.cshtml.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using LTPR.Data;
 using LTPR.Models;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace LTPR.Pages.Admin.Users
 {
@@ -15,7 +17,11 @@ namespace LTPR.Pages.Admin.Users
     {
 
         [BindProperty]
-        public Models.RegistrationModel RegInput { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Pass { get; set; }
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         public string user;
@@ -46,12 +52,17 @@ namespace LTPR.Pages.Admin.Users
                 return NotFound();
             }
 
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             // if user exists, create a reset token and then reset the password using said token
             var user = await _userManager.FindByNameAsync(UserName);
             if (user != null)
             {
                 var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-                await _userManager.ResetPasswordAsync(user, token, RegInput.Password);
+                await _userManager.ResetPasswordAsync(user, token, Pass);
             }
 
             return RedirectToPage("./Index");
